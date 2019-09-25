72477e0d279d9cc0557a44fb0c72e80b6dfbc777
gaswicak <52844928+gaswicak@users.noreply.github.com>
2019-09-25 23:49:01 +0700

    coba bray

--- GetCofactor.java
+++ GetCofactor.java
@@ -3,8 +3,8 @@
     public static double[][] makeEntryMatrix(double[][] M, int a, int b){ // a is for column and b is for column
         double[][] ME = new double[M.length-1][M[0].length-1];
         int k=0, l=0;
-        for(int j = 0; j < M.length; j++){ // j for row
-            for(int i = 0; i<M[0].length; i++){// i for column
+        for(int j = 0; j < M[0].length; j++){ // j for row
+            for(int i = 0; i<M.length; i++){// i for column
                 if((i == a) || (j == b)){
                     continue; // Skip the selected index
                 }else{
@@ -12,7 +12,7 @@
                     l++;
                 }
             }
-            if(i != a){ 
+            if(j != b){ 
                 k++;
             }
         }
@@ -22,8 +22,8 @@
     static double[][] makeCofMatrix(double[][] M){
         double[][] MCof = new double[M.length][M[0].length];
         GetDeterminant myDet = new GetDeterminant();
-        for(int j = 0; j < M.length; j++){
-            for(int i = 0; i<M[0].length; i++){
+        for(int j = 0; j < M[0].length; j++){
+            for(int i = 0; i<M.length; i++){
                 MCof[i][j] = myDet.gaussDeterminant(makeEntryMatrix(M,i,j));
             }
         }
@@ -33,8 +33,8 @@
     static double[][] makeAdjMatrix(double[][] M){
         double[][] AdjM = new double[M.length][M[0].length];
         double temp;
-        for (int j = 0; j < M.length; j++) {
-            for (int i = j; i < M[0].length; i++) {
+        for (int j = 0; j < M[0].length; j++) {
+            for (int i = j; i < M.length; i++) {
                 temp = M[i][j];
                 M[i][j] = M[j][i];
                 M[j][i] = temp;
