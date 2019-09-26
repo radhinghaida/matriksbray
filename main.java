82bf7c8b01b6c51d4e958d29808e067f3ea9bb60
Rainburn <49584704+Rainburn@users.noreply.github.com>
2019-09-26 16:38:32 +0700

    Update main.java

--- main.java
+++ main.java
@@ -23,15 +23,142 @@ class main {
         System.out.println("\n");
         switch (pilihmenu) {
             case 1:
+                GaussEli doGauss = new GaussEli;
                 System.out.println("1. Metode eliminasi gauss");
                 System.out.println("2. Metode eliminasi gauss jordan");
                 System.out.println("3. Metode matriks balikan");
                 System.out.println("4. Kaidah cramer");
-                System.out.println("Mau diapain nih SPLnya?");
+                System.out.print("Mau diapain nih SPLnya? ");
                 Scanner spl = new Scanner(System.in);
                 int pilihspl = spl.nextInt();
+                System.out.println("Matriksnya mau darimana gan ?");
+                System.out.println("1. Input Matriks");
+                System.out.println("2. Baca dari File");
+                int sourceType = spl.nextInt();
+                switch (pilihspl) {
+                    case 1:
+                        if (sourceType == 1) {
+                            int m, n;
+                            System.out.print("Masukkan jumlah baris : ");
+                            n = spl.nextInt();
+                            System.out.print("Masukan jumlah kolom : ");
+                            m = spl.nextInt();
+                            double[][] matriksSPL = new double[m][n];
+                            double[] rightSide = new double[n];
+                            currentMatriks.inputMatriks(matriksSPL, m, n);
+                            currentMatriks.inputRightSide(rightSide, n);
+                            doGauss.GaussEli(matriksSPL, rightSide);
+                            System.out.println("Matriks setelah Gauss-elimination = ");
+                            result.printMatrix(matriksSPL);
+
+                        }
+                        break;
+
+                    case 2:
+                    if (sourceType == 1) {
+                        int m, n;
+                        System.out.print("Masukkan jumlah baris : ");
+                        n = spl.nextInt();
+                        System.out.print("Masukan jumlah kolom : ");
+                        m = spl.nextInt();
+                        double[][] matriksSPL = new double[m][n];
+                        double[] rightSide = new double[n];
+                        currentMatriks.inputMatriks(matriksSPL, m, n);
+                        currentMatriks.inputRightSide(rightSide, n);
+                        doGauss.GaussJordan(matriksSPL, rightSide);
+                        System.out.println("Matriks setelah Gauss-Jordan = ");
+                        result.printMatrix(matriksSPL);
+
+                    }
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                }
+
                 break;
-            case 2:
+
+                
+            case 2: // Determinan
+                GetDeterminant findDet = new Determinant();
+                System.out.println("1. Metode eliminasi gauss");
+                System.out.println("2. Metode Kofaktor");
+                System.out.println("3. Metode Cramer");
+                System.out.print("Mau nyari determinan pake apa bray ? ");
+                Scanner det = new Scanner(System.in);
+                int pilihdet = det.nextInt();
+                System.out.println("Matriksnya mau darimana gan ?");
+                System.out.println("1. Input Matriks");
+                System.out.println("2. Baca dari File");
+                int sourceType = det.nextInt();
+
+                switch (pilihdet) {
+
+                    case 1:
+                        if (sourceType == 1) {
+                            int n;
+                            double valDet;
+                            char answer;
+                            System.out.print("Masukkan N (Matriks N x N) : ");
+                            n = spl.nextInt();
+                            double[][] matriksDet = new double[n][n];
+                            double[][] copyMatDet = new double[n][n];
+                            currentMatriks.inputMatriks(matriksDet, n, n);
+                            copyMatDet = currentMatriks.CopyMatriks(matriksDet);
+                            valDet = findDet.gaussDeterminant(matriksDet);
+                            System.out.println("Determinan dari Matriks diatas adalah = " + valDet);
+                            System.out.println("");
+                            System.out.print("Hasilnya mau disimpen bray (y/n) ? ");
+                            answer = det.next().charAt(0);
+                            if (answer == 'y') {
+                                result.SaveMatriksIntoFile(copyMatDet);
+                                result.SaveDeterminantIntoFile(valDet);
+                            }
+
+                        }
+                        else {
+                            System.out.println("Lagi dibuat sama radhin bray");
+
+                        }
+                        break;
+
+                    case 2:
+                    if (sourceType == 1) {
+                        int n;
+                        double valDet;
+                        char answer;
+                        System.out.print("Masukkan N (Matriks N x N) : ");
+                        n = spl.nextInt();
+                        double[][] matriksDet = new double[n][n];
+                        double[][] copyMatDet = new double[n][n];
+                        currentMatriks.inputMatriks(matriksDet, n, n);
+                        copyMatDet = currentMatriks.CopyMatriks(matriksDet);
+                        valDet = findDet.cofDeterminant(matriksDet);
+                        System.out.println("Determinan dari Matriks diatas adalah = " + valDet);
+                        System.out.println("");
+                        System.out.print("Hasilnya mau disimpen bray (y/n) ? ");
+                        answer = det.next().charAt(0);
+                        if (answer == 'y') {
+                            result.SaveMatriksIntoFile(copyMatDet);
+                            result.SaveDeterminantIntoFile(valDet);
+                        }
+
+                    }
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                }
+
                 break;
             case 3:
                 break;
@@ -40,7 +167,7 @@ class main {
             case 5:
                 break;
             case 6:
-            
+
                 // Interpolasi
                 Interpolasi getInter = new Interpolasi();
                 int jumlahInterpolasi;
