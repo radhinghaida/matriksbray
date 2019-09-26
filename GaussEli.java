2c591d9867d7de516cd3416e8dc6f6d18935bfa3
Rainburn <49584704+Rainburn@users.noreply.github.com>
2019-09-27 01:35:38 +0700

    Update GaussEli.java

--- GaussEli.java
+++ GaussEli.java
@@ -149,12 +149,12 @@ public class GaussEli {
     } // End of isAllZero
 
     static void solveSPL(double[][] A, double[] B) { // Masih bermasalah kalo ada free variable
-        int i, j, totalFreeVar, totalRow, initialASCII;
-        double tempConstant;
+        int i, j, k, l, totalFreeVar, totalRow, initialASCII, loop;
+        double tempConstant, tempCoef;
         boolean thereIsAllZero = false;
         boolean hasSolution = true;
         double[] solutionConst = new double[B.length];
-        double[] coefVar = new double[B.length];
+        double[][] coefVar = new double[B.length][B.length]; // Col buat Partical Solution, row isinya coef dari masing2 parameter (s,t,...)
         char[] variable = new char[B.length];
         char mark = '`';
         totalRow = A[0].length;
@@ -173,9 +173,17 @@ public class GaussEli {
         /* Isi array variable dengan mark, yaitu ` . Isi array solution dengan 0*/
         for (i = 0; i < totalRow; i++) {
             variable[i] = '`';
-            coefVar[i] = 0;
             solutionConst[i] = 0;
         }
+
+        // Isi array coefVar dengan 0 semua
+
+        for (i = 0; i < B.length; i++) {
+            for (j = 0; j < B.length; j++) {
+                coefVar[j][i] = 0;
+            }
+        }
+
 
 
         if (!(hasSolution)) {
@@ -187,6 +195,7 @@ public class GaussEli {
                     totalFreeVar += 1;
                     variable[i] = Character.toUpperCase((char)initialASCII);
                     initialASCII += 1;
+                    coefVar[i][A[0].length - i - 1 ] = 1;
                     System.out.println("X"+ (i+1) + " = " + variable[i]);
                 }
                 else {
@@ -197,6 +206,20 @@ public class GaussEli {
                         for (j = (A.length) - 1; j > i; j--) {
                             if (variable[j] == mark) {
                                 tempConstant -= A[j][i] * solutionConst[j];
+                                for (k = 0; k < totalFreeVar; k++) {
+                                    tempCoef = coefVar[i][k];
+                                    tempCoef -= A[j][i] * coefVar[j][k];
+                                    coefVar[i][k] = tempCoef;
+                                }
+                            }
+                            else {
+
+                                for (k = 0; k < totalFreeVar; k++) {
+                                    tempCoef = coefVar[i][k];
+                                    tempCoef -= A[j][i] * coefVar[j][k];
+                                    coefVar[i][k] = tempCoef;
+                                }
+
                             }
                             solutionConst[i] = tempConstant;
                         }
@@ -204,26 +227,31 @@ public class GaussEli {
                     else {
                         solutionConst[i] = tempConstant;
                     }
-                    // Printing the constant frist 
-                    System.out.print(df3.format(solutionConst[i]));
+                    // Printing the constant first
+                    if (solutionConst[i] != 0) { 
+                        System.out.print(df3.format(solutionConst[i]));
+                    }
 
                     // Printing the variable with the coef, but the coef is still fucked up
-                    for (j = (A.length) - 1; j > i; j--) {
-                        if (variable[j] != mark) {
-                            if (A[j][i] > 0) { // Nilai Elemen positif di matriks
-                                System.out.print(" - " + df3.format(  Math.abs(A[j][i])  ) + variable[j]);
+                            loop = A.length - 1;
+                            for (k = 0; k < totalFreeVar; k++) {
+                                if (coefVar[i][k] > 0) {
+                                    System.out.print(" + " + df3.format(Math.abs(coefVar[i][k])) + variable[loop]);
+                                    loop--;
+                                } 
+                                else if (coefVar[i][k] < 0) {
+                                    System.out.print(" - " + df3.format(Math.abs(coefVar[i][k])) + variable[loop]);
+                                    loop--;
+                                }
+
                             }
-                            else { // Nilai elemen negatif di matriks
-                                System.out.print(" + " + df3.format( Math.abs(A[j][i])  ) + variable[j]);
-                            }
-                        }
-                    }
                     System.out.println("");
                 } // End of Else
             }
         }
-
     } // End of solveSPL
+    
+    
     static double[] cramerSPL(double[][] M){// input augmented matrix
         GetDeterminant myDet = new GetDeterminant();
         double[][] MSmall = new double[M.length-1][M[0].length];
